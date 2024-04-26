420de9ca53f111ad14de89deb64b8c9146fa9c86
Maxwell Swanson <nosnaws1019@gmail.com>
2024-04-25 21:47:08 -0500

    max's addition

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pytesseract
 import boto3
 import requests
 from PIL import Image
+from pytesseract import pytesseract
+import easyocr
+
+reader = easyocr.Reader(['en'])
 
 #client = boto3.client('rekognition', region_name='us-east-1')
 URL = "https://api.ocr.space/parse/image"; 
@@ -12,8 +16,10 @@ def capture_screen(region):
     return pyautogui.screenshot(region=region)
 
 masonPCRegion = (850, 650, (1000-850), (700-650)) #Region to screenshot on Mason's PC
+maxPCRegion = (40, 600, 700, 270,) #Window in top left conner of creen, scrolled all the way up. screen as big as it expands, then stop
 
-screenshot = capture_screen(masonPCRegion)
+#screenshot = capture_screen(masonPCRegion)
+screenshot = capture_screen(maxPCRegion)
 screenshot.save("screenshot.png")
 
 #with open('screenshot.png', 'rb') as f:
@@ -31,5 +37,9 @@ screenshot.save("screenshot.png")
 # AWS!!
 #extracted_text = '\n'.join([text['DetectedText'] for text in response['TextDetections']])
 # Tesseract!!
-extracted_text = pytesseract.image_to_string(screenshot)
+image = Image.open("screenshot.png")
+extracted_text = pytesseract.image_to_string(image)
 print("Extracted text:"+extracted_text)
+
+#result = reader.readtext('screenshot.png', detail=0)
+#print(result)
