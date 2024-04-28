78bb3ac402819f1847d1665f1c5ed8c0b473b9da
Maxwell Swanson <nosnaws1019@gmail.com>
2024-04-28 18:04:38 -0500

    debuging

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ custom_config = r'--oem 3 --psm 6 outputbase digits'
 
 def capture_screen(region):
     shot = pyautogui.screenshot(region=region)
-    #shot.save("View.png")
+    shot.save("View.png")
     return shot
 
 def read_image(image):
@@ -27,41 +27,49 @@ def solve_current_question():
     global answer
     try:
         try:
-            extNum1 = str(abs(int(read_image(capture_screen(c.Region1)))))
+            extNum1 = abs(int(read_image(capture_screen(c.Region1))))
         except ValueError as ve:
-            print("first shot error:"+ve)
+            print("first shot error:", ve)
             extNum1 = 7
         try:
-            extNum2 = str(abs(int(read_image(capture_screen(c.Region2)))))
+            extNum2 = abs(int(read_image(capture_screen(c.Region2))))
         except ValueError as ve:
-            print("second shot error:"+ve)
+            print("second shot error:" , ve)
             extNum2 = 7
         #capture_screen(c.Region1).save("r1.png")
         #capture_screen(c.Region2).save("r2.png")
         print("extNum1:"+extNum1+"ExtNum2:"+extNum2)
-        answer = str(int(extNum1) - int(extNum2))
+        answer = str((extNum1) - (extNum2))
     except ValueError as ve:
         print("ValueError occurred:", ve)
         ValError=True
     except Exception as e:
         print("ERROR:",e,". RETAKING ANSWER SS")
 
-    for x in range(4):
-        ssa = capture_screen(c.answer_regions[x])
-        #ssa.save("SSATEST.png")
-        extAns = pytesseract.image_to_string(ssa, config=custom_config).strip()
+    try:
+        for x in range(4):
+            ssa = capture_screen(c.answer_regions[x])
+            #ssa.save("SSATEST.png")
+            extAns = int(pytesseract.image_to_string(ssa, config=custom_config).strip())
 
-        print("X:",x)
-        print("EXTANS:",extAns)
-        print("ANSWER:",answer)
-        if ((extAns == answer)):
-            clear()
-            print("ANSWER FOUND! BUTTON PRESSED:"+str(x+1))
-            pyautogui.press(str(x+1))
-            return
+            print("X:",x)
+            print("EXTANS:",extAns)
+            print("ANSWER:",answer)
+            if ((extAns == answer)):
+                clear()
+                print("ANSWER FOUND! BUTTON PRESSED:"+str(x+1))
+                pyautogui.press(str(x+1))
+                return
+    except ValueError as ve:
+        print("ValueError occurred:", ve)
+        ValError=True
+    except Exception as e:
+        print("ERROR:",e,". RETAKING ANSWER SS")
+    
 
 while True:
     solve_current_question()
+    time.sleep(1)
     #pyautogui.displayMousePosition()
     
 
